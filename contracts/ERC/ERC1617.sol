547c46f4ef2e187bff8358683a983ea4d514196c
kasoqian <95743792+kasoqian@users.noreply.github.com>
2022-08-18 00:44:55 +0800

    feat: add token exp event before modifier

--- contracts/ERC/ERC1617.sol
+++ contracts/ERC/ERC1617.sol
@@ -75,7 +75,12 @@ contract ERC1617 is EIP1617 {
         subscribeTokens[_tokenID] = block.timestamp;
     }
 
+    function _beforeSubscribeToken(uint _tokenID) internal {
+        if (_isTokenExpire(_tokenID) == false) emit TokenIsExpire(_tokenID);
+    }
+
     modifier onlySubscribeToken(uint _tokenID) {
+        _beforeSubscribeToken(_tokenID);
         require(_isTokenExpire(_tokenID));
         _;
     }
